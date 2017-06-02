b34f34600818e4d826964dacf169b0d36d0763ce
Rohith Sagar Mogili <rohithsagar.14u@gmail.com>
2017-06-02 16:14:48 -0400

    Added Database Exception

--- spring4/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring4/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class App {
 
@@ -12,12 +14,21 @@ public class App {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
 		OffersDAO offersDao = (OffersDAO)context.getBean("offersDao");
-		
-		List<Offer> offers=offersDao.getOffers();
-		System.out.println(offers.size());
-		for(Offer offer:offers){
-			System.out.println(offer);
+		try{
+			List<Offer> offers=offersDao.getOffers();
+			System.out.println(offers.size());
+			for(Offer offer:offers){
+				System.out.println(offer);
+			}
 		}
+		catch(CannotGetJdbcConnectionException ex){
+			System.out.println(ex);
+		}
+		//handles all exceptions related to database
+		catch(DataAccessException ex){
+			System.out.println(ex);
+		}
+		
 		
 		((ClassPathXmlApplicationContext)context).close();
 	}
